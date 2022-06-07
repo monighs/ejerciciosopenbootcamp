dddc146545e688a5530f7fe6c56706b66ed0a3ab
Monica Garcia <monighs1@gmail.com>
2022-06-07 11:47:49 -0500

    subiendo ejercicio 2

---
+++ ejercicio-listas.js
@@ -0,0 +1,30 @@
+// Ejercicio lista
+
+const lista = [
+    'Mónica', 40, false, new Date (1982, 05, 10),
+{
+    titulo:'Crimen y Castigo',
+    autor:'Dostoievski',
+    fecha:1866,
+    url:'https://www.casadellibro.com/libro-crimen-y-castigo/9788491050063/2549214'
+}]
+
+console.log(lista);
+
+
+/*
+const objeto = {
+    nombre: "Mónica",
+    edad: 40,
+    isDev: false,
+    fechaNacimiento: new Date(1982, 05, 10),
+    libroFavorito: {
+        titulo: "Crimen y Castigo",
+        autor: "Dostoievski",
+        fecha:"1866",
+        URL:'https://www.casadellibro.com/libro-crimen-y-castigo/9788491050063/2549214'
+    }
+}
+console.log(objeto);
+
+*/
