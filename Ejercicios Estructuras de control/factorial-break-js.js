a918baebe4ae3dd02c8acba7d19a6319a2df7c64
Monica Garcia <92343259+monighs@users.noreply.github.com>
2022-06-07 20:03:20 -0500

    Add files via upload

---
+++ Ejercicios Estructuras de control/factorial-break-js.js
@@ -0,0 +1,15 @@
+let factorial = 1;
+let numero = 10;
+let i = 1;
+
+while(numero >= 1){
+    
+        factorial *= numero;
+        numero--;
+        if(numero === 0){
+            
+            break;
+        }
+i++
+}
+console.log(factorial)
---
+++ Ejercicios Estructuras de control/factorial-while.js
@@ -0,0 +1,9 @@
+let numero = 10;
+let factorial = 1;
+let i = 1;
+while(i <= numero){
+    factorial *= i;
+    i++;
+}
+
+console.log(factorial)
