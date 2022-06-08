9549e823ae7bf3c9aa8edd9bf5cca8ebb5a02086
Monica Garcia <92343259+monighs@users.noreply.github.com>
2022-06-08 14:34:44 -0500

    Add files via upload

---
+++ Ejercicios Numeros/ejercicios-numeros.js
@@ -0,0 +1,11 @@
+let altura= 161;
+let alturaMetros = 1.61;
+let peso = 48.2;
+let alturaRoundUp = (Math.ceil(alturaMetros));
+console.log(alturaRoundUp);
+
+let pesoRoundDown = (Math.floor(peso));
+console.log(pesoRoundDown);
+
+let maxVal = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+console.log(maxVal)
