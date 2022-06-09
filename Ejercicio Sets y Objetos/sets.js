2f6d9a30c79e1690fa10e62eeff094dea432237b
Monica Garcia <92343259+monighs@users.noreply.github.com>
2022-06-09 16:33:27 -0500

    Ejercicios Sets y Objetos

---
+++ Ejercicio Sets y Objetos/sets.js
@@ -0,0 +1,12 @@
+// - Un nuevo Set con los nombres de tu familia
+
+const miFamilia = ["Martha", "Jorge", "Eduardo", "Mónica", "Bastian", "Marcus"];
+
+const setFamilia = new Set(miFamilia);
+
+console.log(setFamilia);
+
+setFamilia.add("Mónica");
+console.log(setFamilia);
+setFamilia.add("Javascript");
+console.log(setFamilia);
