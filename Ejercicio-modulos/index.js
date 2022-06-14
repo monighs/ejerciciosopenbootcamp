118194386b5249e5df5ddba3e83fe2d89fadd3b9
Monica Garcia <92343259+monighs@users.noreply.github.com>
2022-06-14 17:09:57 -0500

    Ejercicio Modulos

---
+++ Ejercicio-modulos/index.js
@@ -0,0 +1,14 @@
+
+import * as moduloController from './modulos1/controller.js'
+import chalk from 'chalk'
+
+
+const sum = moduloController.suma(4, 5)
+console.log(chalk.green(sum));
+
+const multiply = moduloController.multiplica(1, 2)
+console.log(chalk.green(multiply));
+
+
+
+
